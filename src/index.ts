fff5a9053fbd65070f55d26c55c5f2a0a4872373
alaamahmoud25 <112241976+alaamahmoud25@users.noreply.github.com>
2025-09-20 20:50:02 +0300

    Add /cart/ GET endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,23 @@ import express from 'express';
 import mongoose from 'mongoose';
 import productRoute from './routes/productRoute.js';
 import { seedInitialProducts } from './services/productService.js';
+import cartRoute from './routes/cartRoute.js';
+import userRoute from './routes/userRoute.js';
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
-// Seed the products to database
-seedInitialProducts();
+
 mongoose
   .connect('mongodb://localhost:27017/ecommerce')
   .then(() => console.log('Mongo connected!'))
   .catch((err) => console.log('Failed to connect!', err));
+seedInitialProducts();
 
+app.use('/user', userRoute);
+app.use('/product', productRoute);
+app.use('/cart', cartRoute);
 
 
 app.listen(port, () => {
---
+++ src/models/cartModel.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema, Document } from 'mongoose';
+import type { ObjectId } from 'mongoose';
+import type { IProduct } from './productModel.js';
+
+const CartStatusEnum = ['active', 'completed'];
+
+export interface ICartItem extends Document {
+  product: IProduct;
+  unitPrice: number;
+  quantity: number;
+}
+
+export interface ICart extends Document {
+  userId: ObjectId | string;
+  items: ICartItem[];
+  totalAmount: number;
+  status: 'active' | 'completed';
+}
+
+const cartItemSchema = new Schema<ICartItem>({
+  product: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+  quantity: { type: Number, required: true, default: 1 },
+  unitPrice: { type: Number, required: true },
+});
+
+const cartSchema = new Schema<ICart>({
+  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  items: [cartItemSchema],
+  totalAmount: { type: Number, required: true },
+  status: { type: String, enum: CartStatusEnum, default: 'active' },
+});
+
+export const cartModel = mongoose.model<ICart>('Cart', cartSchema);
